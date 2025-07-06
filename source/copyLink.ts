dbc6a311e81f297e0974d57c14f34747a7fb2caa
Mackenzie Zastrow <zastrowm@users.noreply.github.com>
2025-07-06 14:29:49 -0400

    Get working with manifest v3

--- source/copyLink.ts
+++ source/copyLink.ts
@@ -1,28 +1,47 @@
 // https://www.smashingmagazine.com/2017/04/browser-extension-edge-chrome-firefox-opera-brave-vivaldi/
-import browser from 'webextension-polyfill';
+import * as browser from 'webextension-polyfill';
 
-browser.browserAction.onClicked.addListener(function (tab) {
-	let a = document.createElement('a');
-	a.href = tab.url;
-	a.textContent = tab.title;
+browser.action.onClicked.addListener(async function (tab) {
+	await browser.scripting.executeScript({
+		target: { tabId: tab.id },
+		args: [tab.url, tab.title],
+		func: function callback(url: string, title: string) {
+			let a = document.createElement('a');
+			a.href = url;
+			a.textContent = title;
 
-	// work around apple-notes weird formatting
-	const zeroWidthSpace = `&#xfeff;​`;
+			// work around apple-notes weird formatting
+			const zeroWidthSpace = `&#xfeff;​`;
 
-	const noUnderlineAfter = document.createElement('span');
-	noUnderlineAfter.innerHTML = zeroWidthSpace;
-	noUnderlineAfter.style.textDecoration = 'none';
+			const noUnderlineAfter = document.createElement('span');
+			noUnderlineAfter.innerHTML = zeroWidthSpace;
+			noUnderlineAfter.style.textDecoration = 'none';
 
-	const noUnderlineBefore = document.createElement('span');
-	noUnderlineAfter.innerHTML = ' ';
-	noUnderlineAfter.style.textDecoration = 'none';
+			const noUnderlineBefore = document.createElement('span');
+			noUnderlineAfter.innerHTML = ' ';
+			noUnderlineAfter.style.textDecoration = 'none';
 
-	const markdown = `[${a.textContent}](${a.href})`;
-	const html = noUnderlineBefore.outerHTML + a.outerHTML + noUnderlineAfter.outerHTML;
+			const markdown = `[${a.textContent}](${a.href})`;
+			const html = noUnderlineBefore.outerHTML + a.outerHTML + noUnderlineAfter.outerHTML;
 
-	copyToClipboard({
-		text: markdown,
-		html: html,
+			// from https://stackoverflow.com/a/50067769/548304
+			function copyToClipboard({ text, html }: CopyArgs) {
+				function listener(e) {
+					e.clipboardData.setData('text/html', html);
+					e.clipboardData.setData('text/plain', text);
+					e.preventDefault();
+				}
+
+				document.addEventListener('copy', listener);
+				document.execCommand('copy');
+				document.removeEventListener('copy', listener);
+			}
+
+			copyToClipboard({
+				text: markdown,
+				html: html,
+			});
+		},
 	});
 });
 
@@ -31,17 +50,4 @@ browser.browserAction.onClicked.addListener(function (tab) {
 interface CopyArgs {
 	text: string;
 	html: string;
-}
-
-// from https://stackoverflow.com/a/50067769/548304
-function copyToClipboard({ text, html }: CopyArgs) {
-	function listener(e) {
-		e.clipboardData.setData('text/html', html);
-		e.clipboardData.setData('text/plain', text);
-		e.preventDefault();
-	}
-
-	document.addEventListener('copy', listener);
-	document.execCommand('copy');
-	document.removeEventListener('copy', listener);
 }
