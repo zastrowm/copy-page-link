9d08e97123a8cb7066e5e70f1f1828a6abc7de53
Mackenzie Zastrow <zastrowm@users.noreply.github.com>
2025-07-06 16:09:22 -0400

    Add workaround for github paste behavior
    Pasting into github is broken due to the algorithm for pasting: https://github.com/avo-hq/marksmith/blob/main/app/assets/javascripts/marksmith_controller-no-stimulus.esm.js#L1941 so add a zero width space to the markdown text to avoid the check which applies additional formatting

--- source/copyLink.ts
+++ source/copyLink.ts
@@ -5,49 +5,55 @@ browser.action.onClicked.addListener(async function (tab) {
 	await browser.scripting.executeScript({
 		target: { tabId: tab.id },
 		args: [tab.url, tab.title],
-		func: function callback(url: string, title: string) {
-			let a = document.createElement('a');
-			a.href = url;
-			a.textContent = title;
-
-			// work around apple-notes weird formatting
-			const zeroWidthSpace = `&#xfeff;​`;
-
-			const noUnderlineAfter = document.createElement('span');
-			noUnderlineAfter.innerHTML = zeroWidthSpace;
-			noUnderlineAfter.style.textDecoration = 'none';
-
-			const noUnderlineBefore = document.createElement('span');
-			noUnderlineAfter.innerHTML = ' ';
-			noUnderlineAfter.style.textDecoration = 'none';
-
-			const markdown = `[${a.textContent}](${a.href})`;
-			const html = noUnderlineBefore.outerHTML + a.outerHTML + noUnderlineAfter.outerHTML;
-
-			// from https://stackoverflow.com/a/50067769/548304
-			function copyToClipboard({ text, html }: CopyArgs) {
-				function listener(e) {
-					e.clipboardData.setData('text/html', html);
-					e.clipboardData.setData('text/plain', text);
-					e.preventDefault();
-				}
-
-				document.addEventListener('copy', listener);
-				document.execCommand('copy');
-				document.removeEventListener('copy', listener);
-			}
-
-			copyToClipboard({
-				text: markdown,
-				html: html,
-			});
-		},
+		func: copyPageTitle,
 	});
 });
 
 // TODO https://w3c.github.io/clipboard-apis/#writing-to-clipboard
 
-interface CopyArgs {
-	text: string;
-	html: string;
+function copyPageTitle(url: string, title: string) {
+	let a = document.createElement('a');
+	a.href = url;
+	a.textContent = title;
+
+	const ZWSP = `​`;
+
+	// Work around apple-notes odd-formatting issue that without it it tries to continue formatting
+	const zeroWidthSpace = `&#xfeff;${ZWSP}`;
+
+	const noUnderlineAfter = document.createElement('span');
+	noUnderlineAfter.innerHTML = zeroWidthSpace;
+	noUnderlineAfter.style.textDecoration = 'none';
+
+	const noUnderlineBefore = document.createElement('span');
+	noUnderlineAfter.innerHTML = ' ';
+	noUnderlineAfter.style.textDecoration = 'none';
+
+	// Workaround for GitHub/markdown pasting where they try to reconstruct the markdown. By inserting a ZWSP in the text
+	// content, the plain text isn't found directly in the markdown and thus they don't try to wrap it in extra markdown
+	// ref: https://github.com/avo-hq/marksmith/blob/main/app/assets/javascripts/marksmith_controller-no-stimulus.esm.js#L1992
+	let mdTitle = a.textContent;
+	if (mdTitle.length >= 1) {
+		mdTitle = mdTitle.substring(0, 1) + ZWSP + mdTitle.substring(1);
+	}
+
+	const markdown = `[${mdTitle}](${a.href})`;
+	const html = noUnderlineBefore.outerHTML + a.outerHTML + noUnderlineAfter.outerHTML;
+
+	// from https://stackoverflow.com/a/50067769/548304
+	function copyToClipboard() {
+		function listener(e) {
+			e.clipboardData.setData('text/uri-list', a.href);
+			e.clipboardData.setData('text/html', html);
+			e.clipboardData.setData('text/plain', markdown);
+			e.clipboardData.setData('text/x-markdown', markdown);
+			e.preventDefault();
+		}
+
+		document.addEventListener('copy', listener);
+		document.execCommand('copy');
+		document.removeEventListener('copy', listener);
+	}
+
+	copyToClipboard();
 }
